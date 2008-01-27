a472995064599d2c983760c6659b6d4ef17edb86
Moritz Kroll <moritz.kroll@gmx.de>
2008-01-27 01:36:44 +0000

    Allocate page file buffer in upper memory on GP2X

--- id_pm.cpp
+++ id_pm.cpp
@@ -6,6 +6,51 @@ int PMSoundStart;
 word *PageLengths;
 byte *Pages;
 
+#ifdef GP2x         // Use upper memory on GP2X
+
+void* map_upper_mem()
+{
+    int fd = open("/dev/mem", O_RDWR);
+    if (fd == -1) return 0;
+    void *upper_mem = mmap(0, 32 * 1024 * 1024, PROT_READ | PROT_WRITE,
+        MAP_SHARED, fd, 0x02000000);
+    close(fd);
+    if (upper_mem == MAP_FAILED)
+        return 0;
+    return upper_mem;
+}
+
+void unmap_upper_mem(void *mem)
+{
+    munmap(mem, 32 * 1024 * 1024);
+}
+
+void PML_AllocPagesBuffer(int numChunks)
+{
+    if(numChunks * 4096 > 16 * 1024 * 1024)
+        Quit("Page file needs more than 16 MB!");
+    Pages = (byte *) map_upper_mem();       // use 0x02000000 - 0x03000000 for pages
+}
+
+void PML_FreePagesBuffer()
+{
+    unmap_upper_mem(Pages);
+}
+
+#else
+
+void PML_AllocPagesBuffer(int numChunks)
+{
+    Pages = (byte *) malloc(numChunks * 4096);
+}
+
+void PML_FreePagesBuffer()
+{
+    free(Pages);
+}
+
+#endif
+
 void PM_Startup()
 {
     char fname[13] = "vswap.";
@@ -32,7 +77,7 @@ void PM_Startup()
 
     // TODO: Doesn't support variable page lengths as used by the sounds (page length always <=4096 there)
 
-    Pages = (byte *) malloc(ChunksInFile * 4096);
+    PML_AllocPagesBuffer(ChunksInFile);
     CHECKMALLOCRESULT(Pages);
     for(int i = 0; i < ChunksInFile; i++)
     {
@@ -47,5 +92,5 @@ void PM_Startup()
 void PM_Shutdown()
 {
     free(PageLengths);
-    free(Pages);
+    PML_FreePagesBuffer();
 }
